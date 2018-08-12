a122548f5473a2a038d0bd3bcaf0a03ef1b5a297
sajeevah <sa.hasith@gmail.com>
2018-08-12 18:21:47 +0530

    first commit

---
+++ Converter/DistanceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DistanceViewController.swift
+//  Converter
+//
+//  Created by Admin on 8/12/2561 BE.
+//  Copyright © 2561 BE sajeeva. All rights reserved.
+//
+
+import UIKit
+
+class DistanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
